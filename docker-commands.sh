b98268acdc8f4d5ce1cbc5c26d7eafa4dd7d9686
harshvaghanii <harshvaghani98@gmail.com>
2025-01-11 20:21:09 -0500

    modifying the script file

--- docker-commands.sh
+++ docker-commands.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#Remove the image if exists to rebuild it
+docker rmi feedback-volume
+
 # Build a Docker image
 docker build -t feedback-volume .
 
 # Run a Docker container
-docker run -d -p 3000:80 -v feedback:"/Users/harshvaghani/Desktop/programmingprojects/devops/dummyapps/feedback-app-nodejs:/app.feedback" --name feedback-container --rm feedback-volume
+docker run -d -p 3000:80 -v feedback:"/Users/harshvaghani/Desktop/programmingprojects/devops/dummyapps/feedback-app-nodejs" --name feedback-container --rm feedback-volume
