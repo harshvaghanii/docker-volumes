e21be9ce825eb8057851527d24ef143eff70056c
harshvaghanii <harshvaghani98@gmail.com>
2025-01-11 20:37:56 -0500

    modifying the script

--- docker-commands.sh
+++ docker-commands.sh
@@ -6,5 +6,8 @@ docker rmi feedback-volume
 # Build a Docker image
 docker build -t feedback-volume .
 
+# Deleting the container if exists
+docker stop feedback-container
+
 # Run a Docker container
 docker run -d -p 3000:80 -v feedback:/app/feedback --name feedback-container --rm feedback-volume
